2bd143e33a4172db9f90ced391bc5c9b5f88fcde
Dafeigy <1070642765@qq.com>
2024-05-22 15:18:07 +0800

    Add zh-CN Translation.

---
+++ src/lang/zh-CN.lua
@@ -0,0 +1,128 @@
+---@meta _
+---@diagnostic disable
+
+JowdayDPS.AddLocale('zh-CN', {
+    HeaderText = " DPS / 总伤害: ",
+    AttackText = "攻击",
+    SpecialText = "特技",
+    CastText = "法阵",
+    DashText = "冲刺",
+    WallSlam = "撞墙",
+    EnemyText = "敌人",
+    Charm = "魅惑",
+    -- npc assists
+    ArtemisName = "阿尔忒弥斯",
+    NemesisName = "涅墨西斯",
+    HeraclesName = "赫拉克拉斯",
+    IcarusName = "伊卡洛斯",
+    -- familiars
+    Frinos = "弗利诺斯",
+    Toula = "图拉",
+    -- traps
+    SteamTrap = "蒸汽陷阱",
+    OilFire = "煤油火焰",
+    FireBarrel = "火焰屏障",
+    SpikeTrap = "尖刺陷阱",
+    BrambleTrap = "荆棘陷阱",
+    ThornTrap = "荆棘",
+    Splinters = "Splinters",
+    BeamTrap = "激光",
+    Pillar = "崩塌支柱",
+    Lava = "熔浆",
+    Rubble = "碎石",
+    GenericTrap = "陷阱",
+    --  misc
+    SheepAttack = "羊群攻击",
+    Boulder = "波吕斐摩斯的碎石",
+    ShadeSprint = "巫咒影响",
+    EphyraPylon = "晶石",
+    -- these correspond with Id/DisplayName in TraitText.[lang].sjson or HelpText.[lang].sjson
+    -- aphrodite
+    AphroditeWeaponBoon = "心动打击",
+    AphroditeSpecialBoon = "心动绝技",
+    AphroditeCastBoon = "沦陷之环",
+    AphroditeSprintBoon = "激情冲刺",
+    ManaBurstBoon = "绝情碎心",
+    -- apollo
+    ApolloWeaponBoon = "光耀打击",
+    ApolloSpecialBoon = "光耀绝技",
+    ApolloCastBoon = "烈日之环",
+    ApolloSprintBoon = "炫目奔行",
+    ApolloRetaliateBoon = "热浪袭人",
+    ApolloMissStrikeBoon = "技高一筹",
+    -- artemis
+    SupportingFireBoon = "火力支援",
+    OmegaCastVolleyBoon = "必中标靶",
+    -- demeter
+    DemeterWeaponBoon = "冰霜打击",
+    DemeterSpecialBoon = "冰霜绝技",
+    DemeterCastBoon = "冰雪之环",
+    DemeterSprintBoon = "凛风冲刺",
+    InstantRootKill = "严冬收割",
+    -- hades
+    HadesCastProjectileBoon = "嚎叫之魂",
+    HadesPreDamageBoon = "宿仇旧怨",
+    -- hera
+    HeraWeaponBoon = "盟约打击",
+    HeraSpecialBoon = "盟约绝技",
+    HeraCastBoon = "羁绊之环",
+    HeraSprintBoon = "结缘奔行", -- not currently used
+    DamageShareRetaliateBoon = "恶毒报复",
+    Link = "株连", --HelpText
+    LinkedDeathDamageBoon = "生死与共",
+    -- hestia
+    HestiaWeaponBoon = "烈焰打击",
+    HestiaSpecialBoon = "烈焰绝技",
+    HestiaSprintBoon = "煤烟奔行",
+    HestiaCastBoon = "炙烤之环",
+    BurnExplodeBoon = "野火燎原",
+    Burn = "灼烧", -- HelpText
+    FireballManaSpecialBoon = "掠火流星",
+    CastProjectileBoon = "燃焰焦炭",
+    BurnConsumeBoon = "骤然熄灭",
+    -- hephaestus
+    HephaestusWeaponBoon = "火山打击",
+    HephaestusSpecialBoon = "火山绝技",
+    HephaestusCastBoon = "轰鸣之环",
+    HephaestusSprintBoon = "锤击奔行",
+    MassiveSlam_Name = "赫菲斯托斯的轰击",
+    MassiveKnockupBoon = "熔岩喷涌",
+    -- icarus
+    OmegaExplodeBoon = "爆裂决意",
+    CastHazardBoon = "危险爆破",
+    BreakExplosiveArmorBoon = "灭敌涂层",
+    -- medea
+    DeathDefianceRetaliateCurse = "以怨报怨",
+    SpawnDamageCurse = "所见皆苦",
+    ArmorPenaltyCurse = "所见皆蚀",
+    -- odysseus
+    BossPreDamageKeepsake = "指关节骨",
+    -- poseidon
+    PoseidonWeaponBoon = "浪潮打击",
+    PoseidonSpecialBoon = "浪潮绝技",
+    PoseidonCastBoon = "喷涌之环",
+    PoseidonSprintBoon = "破浪奔行",
+    PoseidonSplash_Name = "波塞冬的海浪",
+    SlamExplosionBoon = "樯倾楫摧",
+    -- selene
+    SpellLaserTrait = "月光射线",
+    SpellLeapTrait = "寂夜狼嚎",
+    SpellMeteorTrait = "月食殆尽",
+    SpellTransformTrait = "黑暗形态",
+    -- zeus
+    ZeusWeaponBoon = "雷霆打击",
+    ZeusSpecialBoon = "雷霆绝技",
+    ZeusSprintBoon = "电掣奔行",
+    ZeusCastBoon = "风暴之环",
+    Echo = "雷霆", -- HelpText
+    FocusLightningBoon = "静电震颤",
+    ZeusManaBoltBoon = "叱咤风云",
+    BoltRetaliateBoon = "神圣复仇",
+    CastAnywhereBoon = "闪电标枪",
+    SpawnKillBoon = "五雷轰顶",
+    -- duo
+    ShadeMercFireballBoon = "燃烧欲望", -- aphrodite x hestia
+    PoseidonSplashSprintBoon = "海滩飞球",  -- apollo x poseidon
+    MassiveCastBoon = "撼地重锤",  -- hephaestus x poseidon
+    SteamBoon = "灼人蒸汽" -- hestia x poseidon
+})
