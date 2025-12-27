eb9e2fbd9e1028415a3933895e6a3ca19be3ad67
Aditya Gupta <adi1998.2010@gmail.com>
2025-12-27 21:50:10 +0530

    get color from the game projectile data table

--- src/scripts/JowdayDPS.Main.lua
+++ src/scripts/JowdayDPS.Main.lua
@@ -747,6 +747,18 @@ function getColorAndLabel(source)
         end
     end
 
+    -- get color from ProjectileData if available as a last resort
+    if color == nil then
+        local projectileData = game.ProjectileData[source]
+        if projectileData ~= nil then
+            if projectileData.DamageTextStartColor ~= nil then
+                color = {
+                    BarColor = projectileData.DamageTextStartColor
+                }
+            end
+        end
+    end
+
     if color == nil then
         color = colors["Default"]
     end
